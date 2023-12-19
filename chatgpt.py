4a43eb47abb42e84306cb80c462ae1a525682713
Chretienb <150497190+Chretienb@users.noreply.github.com>
2023-12-19 14:19:23 -0700

    Update chatgpt.py
    chatbot1

--- chatgpt.py
+++ chatgpt.py
@@ -1,24 +1,164 @@
+import sqlite3
 from openai import OpenAI
 
-client = OpenAI(api_key = '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')
+class ChurchChatbot:
+    def __init__(self, db_file="church_chatbot.db"):
+        self.connection = sqlite3.connect(db_file)
+        self.create_tables()
+        self.prayer_interface = PrayerInterface()
 
+    def create_tables(self):
+        with self.connection:
+            self.connection.execute(
+                """
+                CREATE TABLE IF NOT EXISTS prayers (
+                    id INTEGER PRIMARY KEY,
+                    user_name TEXT,
+                    prayer_request TEXT
+                )
+                """
+            )
+            # Create additional tables for other features if needed
+
+    def submit_prayer_request(self, user_name, prayer_request):
+        with self.connection:
+            self.connection.execute(
+                "INSERT INTO prayers (user_name, prayer_request) VALUES (?, ?)",
+                (user_name, prayer_request)
+            )
+
+    def get_all_prayer_requests(self, user_name):
+        with self.connection:
+            cursor = self.connection.execute("SELECT * FROM prayers WHERE user_name=?", (user_name,))
+            return cursor.fetchall()
+
+    # Implement similar methods for other features...
+
+    def __del__(self):
+        self.connection.close()
+
+class PrayerInterface:
+    def __init__(self):
+        self.prayers = {}
+
+    def submit_prayer_request(self, user_name, prayer_request):
+        if user_name not in self.prayers:
+            self.prayers[user_name] = []
+        self.prayers[user_name].append(prayer_request)
+
+    def get_prayer_requests(self, user_name):
+        if user_name in self.prayers:
+            return self.prayers[user_name]
+        else:
+            return []
+
+# Example Usage:
+church_chatbot = ChurchChatbot()
 
 messages = [
-   {"role": "system", "content": "LE GLOBE"}
+    {"role": "system", "content": "You are a helpful assistant for a church."},
 ]
+
+print("Welcome to Centerpoint Church Chatbot! Type 'Q' to exit.")
 user_input = input()
-while user_input != 'q':
 
+while user_input.lower() != 'q':
+    # User input
+    messages.append({"role": "user", "content": user_input})
 
-   messages.append({"role": "user", "content": user_input})
-   completion = client.chat.completions.create(
-       model = 'gpt-3.5-turbo',
-       messages=messages
-   )
-   messages.append(completion.choices[0].message)
-   print()
-   print(completion.choices[0].message.content)
-   print()
-   user_input = input()
+    # Handle different features
+    if "prayer" in user_input.lower():
+        # Handle Prayer Requests
+        user_name = "John"  # Replace with the actual user's name (you might get this from the user profile)
+        prayer_request = " ".join(user_input.split()[1:])  # Extract the prayer request from the user input
+        response = "Thank you for submitting your prayer request. Our community will keep you in our prayers."
+        
+        # Store the prayer request in the database
+        church_chatbot.submit_prayer_request(user_name, prayer_request)
+
+    elif "get prayers" in user_input.lower():
+        # Retrieve and display user's prayer requests
+        user_name = "John"  # Replace with the actual user's name
+        prayer_requests = church_chatbot.get_all_prayer_requests(user_name)
+        response = f"Your prayer requests: {', '.join([prayer[2] for prayer in prayer_requests])}"
+
+    elif "event" in user_input.lower():
+        # Handle Event Information
+        # Implement logic to fetch and display upcoming events
+        response = "Here are the upcoming church events: [Event 1, Event 2, ...]"
+
+    elif "sermon" in user_input.lower():
+        # Handle Sermon Archives
+        # Implement logic to provide access to past sermons
+        response = "You can access past sermons on our website or through our podcast platform."
+
+    elif any(keyword in user_input.lower() for keyword in ["donation", "tithing"]):
+        # Handle Donations and Tithing
+        # Implement logic to facilitate online donations
+        response = "Thank you for your generosity! You can make online donations through our website."
+
+    elif "membership" in user_input.lower():
+        # Handle Membership Information
+        # Implement logic to provide information about membership programs
+        response = "Learn about our membership programs and classes on our website or contact our membership coordinator."
+
+    elif any(keyword in user_input.lower() for keyword in ["bible", "devotional"]):
+        # Handle Bible Verses and Devotionals
+        # Implement logic to deliver daily or weekly Bible verses
+        response = "Here is a Bible verse to inspire you: [Verse]. You can also find daily devotionals on our website."
+
+    elif "volunteer" in user_input.lower():
+        # Handle Volunteer Opportunities
+        # Implement logic to highlight ongoing volunteer opportunities
+        response = "Explore our volunteer opportunities and make a positive impact in our community. Contact our volunteer coordinator for details."
+
+    elif any(keyword in user_input.lower() for keyword in ["faq", "general information"]):
+        # Handle FAQs and General Information
+        # Implement logic to answer frequently asked questions
+        response = "Visit our FAQ page on the website for answers to common questions. If you need further assistance, feel free to ask."
+
+    elif any(keyword in user_input.lower() for keyword in ["community", "engagement"]):
+        # Handle Community Engagement
+        # Implement logic to foster community engagement
+        response = "Join our community discussions and share your thoughts and testimonies. Your engagement is important to us!"
+
+    elif "social media" in user_input.lower():
+        # Handle Integration with Social Media
+        # Implement logic to connect the chatbot with the church's social media accounts
+        response = "Connect with us on social media to stay updated on events, share content, and engage with the community."
+
+    elif any(keyword in user_input.lower() for keyword in ["announcement", "emergency"]):
+        # Handle Special Announcements
+        # Implement logic to send out important announcements
+        response = "Important announcement: [Announcement]. Stay tuned for further updates."
+
+    elif any(keyword in user_input.lower() for keyword in ["feedback", "survey"]):
+        # Handle Feedback and Surveys
+        # Implement logic to gather feedback through surveys
+        response = "We value your feedback! Participate in our surveys to help us improve and better serve the community."
+
+    else:
+        # Default response for other queries
+        completion = client.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        response = completion.choices[0].message['content']
+
+    # Append chatbot response to the conversation
+    messages.append({"role": "assistant", "content": response})
+
+    # Display chatbot response
+    print()
+    print(response)
+    print()
+
+    # Get user input for the next iteration
+    user_input = input()
+
+# ... (rest of the code)
+
+print("Goodbye! Chatbot session ended.")
+
 
 
